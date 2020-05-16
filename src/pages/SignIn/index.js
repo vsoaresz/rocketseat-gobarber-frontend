7bd23130ab2b5781f2dd68cda196b9c728c86508
Vitor Soares <vitor@Vitors-MacBook-Pro.local>
2020-05-16 02:05:54 -0300

    finalizando aula 11

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,9 +1,56 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { Form } from '@unform/web';
+import { Link } from 'react-router-dom';
+import * as Yup from 'yup';
 
-// import { Container } from './styles';
+import Input from '~/components/Form/Input';
+
+import logo from '~/assets/logo.svg';
 
 function SignIn() {
-  return <h1>Login</h1>;
+  const formRef = useRef(null);
+
+  async function handleSubmit(data, { reset }) {
+    try {
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .email('Insira um e-mail válido.')
+          .required('O e-mail é obrigatório.'),
+        password: Yup.string()
+          .min(3, 'minimo 3 caracteres.')
+          .required('A senha é obrigatória.'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+      reset();
+    } catch (err) {
+      const validationErrors = {};
+
+      if (err instanceof Yup.ValidationError) {
+        err.inner.forEach((error) => {
+          validationErrors[error.path] = error.message;
+        });
+
+        formRef.current.setErrors(validationErrors);
+      }
+    }
+  }
+
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <Form ref={formRef} onSubmit={handleSubmit}>
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input name="password" type="password" placeholder="Sua senha" />
+
+        <button type="submit">Acessar</button>
+        <Link to="/register">Criar conta gratuita</Link>
+      </Form>
+    </>
+  );
 }
 
 export default SignIn;
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,9 +1,57 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { Link } from 'react-router-dom';
+import { Form } from '@unform/web';
+import * as Yup from 'yup';
+import Input from '~/components/Form/Input';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.svg';
 
 function SignUp() {
-  return <div />;
+  const formRef = useRef(null);
+
+  async function handleSubmit(data, { reset }) {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required('O nome é obrigatório'),
+        email: Yup.string()
+          .email('Insira um e-mail válido.')
+          .required('O e-mail é obrigatório.'),
+        password: Yup.string()
+          .min(3, 'minimo 3 caracteres.')
+          .required('A senha é obrigatória.'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+      reset();
+    } catch (err) {
+      const validationErrors = {};
+
+      if (err instanceof Yup.ValidationError) {
+        err.inner.forEach((error) => {
+          validationErrors[error.path] = error.message;
+        });
+
+        formRef.current.setErrors(validationErrors);
+      }
+    }
+  }
+
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <Form ref={formRef} onSubmit={handleSubmit}>
+        <Input name="name" type="text" placeholder="Nome Completo" />
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input name="password" type="password" placeholder="Sua senha" />
+
+        <button type="submit">Criar conta</button>
+        <Link to="/">Já tenho login</Link>
+      </Form>
+    </>
+  );
 }
 
 export default SignUp;
